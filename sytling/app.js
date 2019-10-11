17be8e5d9e853011f6203e725f6c22198608f7fe
Hani Fares <h.fares@ofyz.de>
2019-10-11 14:47:35 +0200

    lesson  28

--- sytling/app.js
+++ sytling/app.js
@@ -4,5 +4,12 @@ new Vue ({
         attachRed : false,
         attachGreen : false,
         attachBlue : false
+    },
+    computed : {
+        divClasses : function () {
+            return {
+                green : this.attachGreen , blue : !this.attachBlue
+            }
+        }
     }
 });
