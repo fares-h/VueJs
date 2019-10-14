5f42ceb6837f6ddb0b205dd0f5b1f50b55a6c5ec
Hani Fares <h.fares@ofyz.de>
2019-10-14 09:42:26 +0200

    lesson 39

--- ConditionalsAndRendering/app.js
+++ ConditionalsAndRendering/app.js
@@ -2,6 +2,13 @@
 new Vue ({
    el: '#app',
    data : {
-       seen : false
+       seen : false,
+       ingredients: ['meat', 'fruit', 'cookies'],
+       persons : [
+           {name : 'Max', age : 27, color : 'red'},
+           {name : 'Hani', age : 26, color : 'blue'},
+           {name : 'Rami', age : 24, color : 'black'},
+           {name : 'Maria', age : 19, color : 'white'}
+       ]
    }
 });
