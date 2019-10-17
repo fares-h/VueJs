eb4dc4b263d2688d043f2c974442d68e3f486bbd
Hani Fares <h.fares@ofyz.de>
2019-10-17 08:46:52 +0200

    Exercise 8 part 2

--- exercise8/src/App.vue
+++ exercise8/src/App.vue
@@ -3,25 +3,13 @@
         <div class="row">
             <div class="col-xs-12">
                 <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
+                <button @click="selectComponent('blue')" class="btn btn-primary">Load Blue Template</button>
+                <button @click="selectComponent('green')" class="btn btn-success">Load Green Template</button>
+                <button @click="selectComponent('red')" class="btn btn-danger">Load Red Template</button>
                 <hr>
-                <app-blue>
-                    <div>
-                        I'm BLUE
-                    </div>
-                </app-blue>
-                <app-green>
-                    <app-red>
-                        <app-blue>
-                            <div>
-                                Im cool
-                            </div>
-                        </app-blue>
-                    </app-red>
-                </app-green>
-                <app-red></app-red>
+                <component :is="componentSelected">
+                    <p>HI I'M CONTENT</p>
+                </component>
             </div>
         </div>
     </div>
@@ -37,6 +25,21 @@
             appBlue: Blue,
             appGreen: Green,
             appRed: Red
+        },
+        data () {
+            return {
+                componentSelected: 'appRed'
+            }
+        },
+        methods: {
+            selectComponent: function (color) {
+                if(color === 'blue')
+                    this.componentSelected = 'appBlue';
+                if(color === 'green')
+                    this.componentSelected = 'appGreen';
+                if(color === 'red')
+                    this.componentSelected = 'appRed';
+            }
         }
     }
 </script>
