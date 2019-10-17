21a2cf1cb7a53afc90152d33689c61c1313b2726
Hani Fares <h.fares@ofyz.de>
2019-10-17 15:51:30 +0200

    Wonderful Quotes Max Solution

--- WonderfulQuotesSolution/src/App.vue
+++ WonderfulQuotesSolution/src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
         <app-new-quote @quoteAdded="newQuote"></app-new-quote>
         <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
         <div class="row">
@@ -13,6 +14,8 @@
 <script>
     import QuoteGrid from './components/QuoteGrid';
     import NewQuote from './components/NewQuote';
+    import Header from './components/Header';
+
     export default {
         data: function () {
             return {
@@ -25,9 +28,13 @@
         components: {
             appQuoteGrid: QuoteGrid,
             appNewQuote: NewQuote,
+            appHeader: Header,
         },
         methods: {
             newQuote(quote) {
+                if(this.quotes.length >= this.maxQuotes) {
+                    return alert('Please delete a Quote')
+                }
                 this.quotes.push(quote);
             },
             deleteQuote(index) {
